0de2b78e76aedafd60a5bd675107feca654e5276
rachelklee <77082379+rachelklee@users.noreply.github.com>
2021-11-19 10:32:26 -0800

    background color (index.html and nav.html)

---
+++ src/main/java/com/example/sping_portfolio/rachelCollegeBoardFRQs/LightSequence.java
@@ -0,0 +1,57 @@
+package com.example.sping_portfolio.rachelCollegeBoardFRQs;
+
+// OBJECTIVE:  Write a statement to create a LightSequence object gradShow that has the initial light sequence "0101 0101 0101".
+
+public class LightSequence
+
+{
+
+// attributes not shown
+
+
+
+    /** The parameter seq is the initial sequence used for
+
+     * the light display
+
+     */
+
+    public LightSequence(String seq)
+
+    { /* implementation not shown */ }
+
+
+
+    /** Inserts the string segment in the current sequence,
+
+     * starting at the index ind. Returns the new sequence.
+
+     */
+
+    public void insertSegment(String segment, int ind)
+
+    { /* implementation not shown */ }
+
+
+
+    /** Updates the sequence to the value in seq
+
+     */
+
+    public void changeSequence(String seq)
+
+    { /* implementation not shown */ }
+
+
+
+    /** Uses the current sequence to turn the light on and off
+
+     * for the show
+
+     */
+
+    public void display()
+
+    { /* implementation not shown */ }
+
+}
