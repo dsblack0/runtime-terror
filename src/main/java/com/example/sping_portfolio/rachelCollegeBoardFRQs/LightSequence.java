e77e05e2d4be95806b4a8ec2287a0da969581d9d
rachelklee <iamrachelklee@gmail.com>
2021-12-05 22:21:28 -0800

    v0

--- src/main/java/com/example/sping_portfolio/rachelCollegeBoardFRQs/LightSequence.java
+++ src/main/java/com/example/sping_portfolio/rachelCollegeBoardFRQs/LightSequence.java
@@ -1,55 +1,58 @@
 package com.example.sping_portfolio.rachelCollegeBoardFRQs;
 
-// OBJECTIVE:  Write a statement to create a LightSequence object gradShow that has the initial light sequence "0101 0101 0101".
-
-import groovy.lang.GString;
+import java.lang.Math;
 
 public class LightSequence
 {
-// attributes not shown
-    public LightSequence gradShow;
-
+    private String resultSeq;
 
     public LightSequence(String seq)
     {
-        LightSequence gradShow = new LightSequence("0101 0101 0101");
-        gradShow.display();
-        gradShow.changeSequence("0011 0011 0011");
+        resultSeq = seq;
+        System.out.println("LightSequence");
     }
-    /** Inserts the string segment in the current sequence,
 
-     * starting at the index ind. Returns the new sequence.
-
-     */
-
-    public void insertSegment(String segment, int ind)
-
+    public String insertSegment(String segment, int ind)
     {
-        gradShow.insertSegment("1111 1111", 4);
+        resultSeq = resultSeq.substring(0,ind+1) + segment + resultSeq.substring(ind+1);
+        return resultSeq;
     }
-
-
-
-    /** Updates the sequence to the value in seq
-
-     */
 
     public void changeSequence(String seq)
-
-    { /* implementation not shown */ }
-
-
-
-    /** Uses the current sequence to turn the light on and off
-
-     * for the show
-     * @param gradShow
-
-     */
+    {
+        resultSeq = seq;
+        System.out.println("changeSequence");
+    }
 
     public void display()
     {
-        return;
+        System.out.println(resultSeq);
     }
 
+    public static String questionE(String oldSeq, String oldSegment, String newSegment)
+    {
+        String newSeq = oldSeq.replaceFirst(oldSegment, newSegment);
+        System.out.println(newSeq);
+        return newSeq;
+    }
+
+    public static void questionF(double a, double b)
+    {
+        double c = Math.sqrt(a*a + b * b);
+        System.out.println(c);
+    }
+
+    public static void main(String []args)
+    {
+        String newSeq;
+        LightSequence gradShow = new LightSequence("0101 0101 0101");
+        gradShow.display();
+        gradShow.changeSequence("0011 0011 0011");
+        gradShow.insertSegment("1111 1111", 4);
+        gradShow.display();
+        newSeq = questionE("110000011", "11","");
+        newSeq = questionE("0000011", "11","");
+        newSeq = questionE("1100000111","00","");
+        questionF(10.4,21.3);
+    }
 }
