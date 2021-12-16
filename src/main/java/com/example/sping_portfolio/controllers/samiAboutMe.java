b33a9b36f927b3b13c312116459487a3719296fe
sam <70492417+dsblack0@users.noreply.github.com>
2021-12-16 06:42:57 -0800

    fixed sami's aboutme code

--- src/main/java/com/example/sping_portfolio/controllers/samiAboutMe.java
+++ src/main/java/com/example/sping_portfolio/controllers/samiAboutMe.java
@@ -1,6 +1,5 @@
-package com.example.sping_portfolio.controllers.samiAboutMe;
+package com.example.sping_portfolio.controllers;
 
-import com.example.sping_portfolio.controllers.samAboutMe.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
