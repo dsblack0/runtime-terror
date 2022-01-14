31a829717cb1bf640cc109b106cc0f554efe23f9
sam <70492417+dsblack0@users.noreply.github.com>
2022-01-14 09:09:30 -0800

    fixed issue in kaavya's frq5

--- src/main/java/com/example/sping_portfolio/kaavyaCollegeBoardFRQs/frq5.java
+++ src/main/java/com/example/sping_portfolio/kaavyaCollegeBoardFRQs/frq5.java
@@ -4,10 +4,7 @@ import com.example.sping_portfolio.kaavyaCollegeBoardFRQs.*;
 import com.example.sping_portfolio.kaavyaCollegeBoardFRQs.LightSequence;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
