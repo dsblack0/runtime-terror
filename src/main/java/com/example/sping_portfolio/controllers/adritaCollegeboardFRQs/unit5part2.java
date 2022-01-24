8f3d56e04be034af779c8682fd2fc853be9624c2
adritac-tech <adrita.chakraborty@outlook.com>
2022-01-24 00:18:14 -0800

    unit 5 part 2 finished

--- src/main/java/com/example/sping_portfolio/controllers/adritaCollegeboardFRQs/unit5part2.java
+++ src/main/java/com/example/sping_portfolio/controllers/adritaCollegeboardFRQs/unit5part2.java
@@ -3,12 +3,24 @@ package com.example.sping_portfolio.controllers.adritaCollegeboardFRQs;
 import java.util.Random;
 
 public class unit5part2 {
-    int count;
+    int count = 0;
     String prefix;
     int digits;
     public unit5part1(int digits, String prefix){
         this.digits = digits;
         this.prefix = prefix;
     }
-
+    public unit5part1(int digits){
+        this.digits = digits;
+        prefix = "A";
+    }
+    public String pwGenerator(){
+        count += 1;
+        String pwd = this.prefix + ".";
+        for (int i = 1; i <= this.length; i++) {
+            int randomNum = new Random().nextInt(10);
+            pwd += randomNum;
+        }
+        return pwd;
+    }
 }
