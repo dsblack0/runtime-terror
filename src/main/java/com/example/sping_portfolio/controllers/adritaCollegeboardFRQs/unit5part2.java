f0d6d856e016a2f896a1f6af7b7e5b4b5647dcfb
adritac-tech <adrita.chakraborty@outlook.com>
2022-01-24 00:14:41 -0800

    unit 5 part 2

--- src/main/java/com/example/sping_portfolio/controllers/adritaCollegeboardFRQs/unit5part2.java
+++ src/main/java/com/example/sping_portfolio/controllers/adritaCollegeboardFRQs/unit5part2.java
@@ -1,4 +1,14 @@
 package com.example.sping_portfolio.controllers.adritaCollegeboardFRQs;
 
+import java.util.Random;
+
 public class unit5part2 {
+    int count;
+    String prefix;
+    int digits;
+    public unit5part1(int digits, String prefix){
+        this.digits = digits;
+        this.prefix = prefix;
+    }
+
 }
