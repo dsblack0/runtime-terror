5c15321b2c10d9381db31a149c563988c0df1d3d
adritac-tech <adrita.chakraborty@outlook.com>
2022-01-23 23:33:32 -0800

    unit 3

--- src/main/java/com/example/sping_portfolio/controllers/adritaCollegeboardFRQs/unit3.java
+++ src/main/java/com/example/sping_portfolio/controllers/adritaCollegeboardFRQs/unit3.java
@@ -9,6 +9,36 @@ public class unit3 {
         person = name;
     }
     public String attendance(boolean rsvp){
-
+        if (rsvp == true){
+            String attendance = "attending";
+            System.out.println(attendance);
+            return attendance;
+        }
+        else {
+            String attendance = "not attending";
+            System.out.println(attendance);
+            return attendance;
+        }
     }
+    public String food(int selection){
+        String food;
+        if (selection == 1){
+            food = "beef";
+            System.out.println(food);
+        }
+        else if (selection == 2){
+            food = "chicken";
+            System.out.println(food);
+        }
+        else if (selection == 3){
+            food = "pasta";
+            System.out.println(food);
+        }
+        else{
+            food = "fish";
+            System.out.println(food);
+        }
+        return food;
+    }
+
 }
