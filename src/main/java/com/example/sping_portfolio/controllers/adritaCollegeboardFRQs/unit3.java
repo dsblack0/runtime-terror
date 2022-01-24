5f90f2977342fdc1a3101422b18e3931a860cc3a
adritac-tech <adrita.chakraborty@outlook.com>
2022-01-23 23:14:32 -0800

    unit 3

--- src/main/java/com/example/sping_portfolio/controllers/adritaCollegeboardFRQs/unit3.java
+++ src/main/java/com/example/sping_portfolio/controllers/adritaCollegeboardFRQs/unit3.java
@@ -1,4 +1,14 @@
 package com.example.sping_portfolio.controllers.adritaCollegeboardFRQs;
 
 public class unit3 {
+    String person;
+    boolean rsvp;
+    int selection;
+
+    public unit3(String name){
+        person = name;
+    }
+    public String attendance(boolean rsvp){
+
+    }
 }
