87d21425ca82d5e9e9a2a2a0e77ad5bf50bc3ed2
rachelklee <iamrachelklee@gmail.com>
2022-02-05 23:52:27 -0800

    frq html

--- src/main/java/com/example/sping_portfolio/rachelCollegeBoardFRQs/NumberSystem.java
+++ src/main/java/com/example/sping_portfolio/rachelCollegeBoardFRQs/NumberSystem.java
@@ -6,7 +6,9 @@ public class NumberSystem {
      * Returns the greatest common factor of a and b, as described in part (a).
      */
 
-    public static int gcf(int a, int b){
+    public String str;
+
+    public int gcf(int a, int b){
         int num1 = a;
         int num2 = b;
         int gcf=1;
@@ -30,28 +32,49 @@ public class NumberSystem {
      * and prints the result, as described in part (b).
      */
 
-    public static void reduceFraction(int numerator, int denominator){
+    public void reduceFraction(int numerator, int denominator){
         int num = numerator;
         int den = denominator;
-        String reduced = "0";
-
+        String reduced;
         if (num%den==0){
-            reduced=num + "/" + den;
-            System.out.println(num + "/" + den + "reduces to " + reduced);
+            reduced=Integer.toString(num/den);
+            str = num + "/" + den + " reduces to " + reduced;
+            System.out.println(num + "/" + den + " reduces to " + reduced);
         }
         else{
             int gcf = gcf(num,den);
             reduced = (num/gcf) + "/" + (den/gcf);
-            System.out.println(num + "/" + den + "reduces to " + reduced);
+            str = num + "/" + den + " reduces to " + reduced;
+            System.out.println("str: " + str);
+            System.out.println(num + "/" + den + " reduces to " + reduced);
         }
+
 
     }
 
+    public String output(){
+        System.out.println(str);
+        return str;
+    }
+
+
+
     public static void main(String[] args) {
-        reduceFraction(30,3);
-        reduceFraction(8,20);
-        reduceFraction(24,9);
-        reduceFraction(7,3);
-        reduceFraction(10,6);
+
+        NumberSystem r1 = new NumberSystem();
+        NumberSystem r2 = new NumberSystem();
+        NumberSystem r3 = new NumberSystem();
+        NumberSystem r4 = new NumberSystem();
+        NumberSystem r5 = new NumberSystem();
+        r1.reduceFraction(30,3);
+        r2.reduceFraction(8,20);
+        r3.reduceFraction(24,9);
+        r4.reduceFraction(7,3);
+        r5.reduceFraction(10,6);
+
+        System.out.println("TEST");
+        r1.output();
+
+
     }
 }
