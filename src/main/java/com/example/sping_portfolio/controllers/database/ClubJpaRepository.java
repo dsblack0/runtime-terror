05c091f2c84c59219732ed1c7d9233fa094f6b49
adritac-tech <adrita.chakraborty@outlook.com>
2022-02-11 00:20:58 -0800

    finished jpa rep

--- src/main/java/com/example/sping_portfolio/controllers/database/ClubJpaRepository.java
+++ src/main/java/com/example/sping_portfolio/controllers/database/ClubJpaRepository.java
@@ -1,4 +1,18 @@
 package com.example.sping_portfolio.controllers.database;
 
-public class ClubJpaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.persistence.QueryHint;
+import java.util.List;
+
+public interface ClubJpaRepository extends JpaRepository<Club, Long> {
+    List<Club> findClubName(String name, String president);
+
+    @Query(
+            value = "SELECT * FROM Club p WHERE p.name LIKE ?1 or p.president LIKE ?1",
+            nativeQuery = true)
+
+    List<Club> findByLikeTermNative(String term);
+
 }
