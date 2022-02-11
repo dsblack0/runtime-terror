7c0e72fa052faa7b73c8b1ccb4a822746f0b8ea5
adritac-tech <adrita.chakraborty@outlook.com>
2022-02-11 00:37:58 -0800

    club.java amends

--- src/main/java/com/example/sping_portfolio/controllers/database/Club.java
+++ src/main/java/com/example/sping_portfolio/controllers/database/Club.java
@@ -5,6 +5,7 @@ import lombok.*;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
 
 @Setter
 @Getter
@@ -16,6 +17,13 @@ import javax.persistence.Id;
 public class Club {
     @Id
     @GeneratedValue()
+    private Long id;
+    @NotEmpty
+    @Size(min=5)
+
+    @NonNull
+    @Size(min = 2, max = 30, message = "Name (2-30 characters)")
+    private String name;
 
     public Club(String name, String president, int memberCount){
         this.name = name;
@@ -23,7 +31,9 @@ public class Club {
         this.memberCount = memberCount;
     }
 
-    public int getMemberCount(){
-
-    }
+//    public int getMemberCount(){
+//        if(this.memberCount != null){
+//            //modify date code to this case
+//        }
+//    }
 }
--- src/main/java/com/example/sping_portfolio/controllers/database/ClubJpaRepository.java
+++ src/main/java/com/example/sping_portfolio/controllers/database/ClubJpaRepository.java
@@ -1,6 +1,6 @@
 package com.example.sping_portfolio.controllers.database;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository; //check these
 import org.springframework.data.jpa.repository.Query;
 
 import javax.persistence.QueryHint;
@@ -9,7 +9,7 @@ import java.util.List;
 public interface ClubJpaRepository extends JpaRepository<Club, Long> {
     List<Club> findClubName(String name, String president);
 
-    @Query(
+    @Query( //check this
             value = "SELECT * FROM Club p WHERE p.name LIKE ?1 or p.president LIKE ?1",
             nativeQuery = true)
 
