a7284ff713492946dbb2fa2af64eccfe2e7155c3
adritac-tech <adrita.chakraborty@outlook.com>
2022-02-11 00:16:19 -0800

    club controller

--- src/main/java/com/example/sping_portfolio/controllers/database/ClubController.java
+++ src/main/java/com/example/sping_portfolio/controllers/database/ClubController.java
@@ -1,4 +1,32 @@
 package com.example.sping_portfolio.controllers.database;
+import com.example.sping_portfolio.controllers.database.Club;
+import com.example.sping_portfolio.controllers.database.ClubSqlRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class ClubController {
+import org.springframework.http.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.validation.Valid;
+import java.text.SimpleDateFormat;
+
+import java.util.*;
+
+@Controller
+public class ClubController implements WebMvcConfigurer {
+    @Autowired
+    private ClubSqlRepository repository;
+
+    @GetMapping("/database")
+    public String club(Model model){
+        List<Club> list = repository.listAll();
+        model.addAttribute("list", "list");
+        return "/database";
+    }
+
+    @GetMapping("")
+
 }
