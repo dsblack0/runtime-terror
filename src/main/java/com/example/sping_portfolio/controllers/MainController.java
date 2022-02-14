ee17bfa8264da1f31a3a947509a53750c82b7b21
sam <70492417+dsblack0@users.noreply.github.com>
2022-02-14 10:37:26 -0800

    added view formatting for reviewsPage

--- src/main/java/com/example/sping_portfolio/controllers/MainController.java
+++ src/main/java/com/example/sping_portfolio/controllers/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 @Controller
 public class MainController {
@@ -27,14 +28,30 @@ public class MainController {
     }
 
     @GetMapping ("/reviewsPage")
-    public String reviewsPage(@RequestParam(name="clubName", required = false, defaultValue = "Club Name") String clubName,
-                              @RequestParam(name="reviewText",required = false, defaultValue = "review text") String reviewText,
+    public String reviewsPage(@RequestParam(name="clubName", required = false, defaultValue = "") String clubName,
+                              @RequestParam(name="reviewText",required = false, defaultValue = "") String reviewText,
                               @RequestParam(name="reviewerName", required = false, defaultValue = "") String reviewerName, Model model) {
 
+        reviewsPage review5 = new reviewsPage();
+        review5.reviewCreate(clubName, reviewText, reviewerName);
+
         reviewsPage review1 = new reviewsPage();
-        review1.reviewCreate(clubName, reviewText, reviewerName);
+        review1.reviewCreate("Robotics", "This is some random default review text that is ment mainly to just test this out because I don't know what to set", "");
+        reviewsPage review2 = new reviewsPage();
+        review2.reviewCreate("Speech & Debate", "This is some random default review text that is ment mainly to just test this out because I don't know what to set", "a person");
+        reviewsPage review3 = new reviewsPage();
+        review3.reviewCreate("Math Club", "This is some random default review text that is ment mainly to just test this out because I don't know what to set", "");
+        reviewsPage review4 = new reviewsPage();
+        review4.reviewCreate("Key Club", "This is some random default review text that is ment mainly to just test this out because I don't know what to set", "");
+
         ArrayList<reviewsPage> reviews = new ArrayList<>();
         reviews.add(review1);
+        reviews.add(review2);
+        reviews.add(review3);
+        reviews.add(review4);
+        if(!Objects.equals(clubName, "")) {
+            reviews.add(review5);
+        }
 
         model.addAttribute("reviews", reviews);
 
