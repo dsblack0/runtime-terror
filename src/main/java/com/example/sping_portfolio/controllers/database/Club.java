36cbb8eaa4685e797ea87068052281fea0839fe5
adritac-tech <adrita.chakraborty@outlook.com>
2022-02-14 22:01:27 -0800

    commented out db stuff

--- src/main/java/com/example/sping_portfolio/controllers/database/Club.java
+++ src/main/java/com/example/sping_portfolio/controllers/database/Club.java
@@ -1,39 +1,40 @@
-package com.example.sping_portfolio.controllers.database;
-
-import lombok.*;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.validation.constraints.NotEmpty;
-
-@Setter
-@Getter
-@ToString
-@NoArgsConstructor
-@Entity
-@RequiredArgsConstructor
-
-public class Club {
-    @Id
-    @GeneratedValue()
-    private Long id;
-    @NotEmpty
-    @Size(min=5)
-
-    @NonNull
-    @Size(min = 2, max = 30, message = "Name (2-30 characters)")
-    private String name;
-
-    public Club(String name, String president, int memberCount){
-        this.name = name;
-        this.president = president;
-        this.memberCount = memberCount;
-    }
-
-//    public int getMemberCount(){
-//        if(this.memberCount != null){
-//            //modify date code to this case
-//        }
+//package com.example.sping_portfolio.controllers.database;
+//
+//import lombok.*;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.Id;
+//import javax.validation.constraints.NotEmpty;
+//
+//@Setter
+//@Getter
+//@ToString
+//@NoArgsConstructor
+//@Entity
+//@RequiredArgsConstructor
+//
+//public class Club {
+//    @Id
+//    @GeneratedValue()
+//    private Long id;
+//
+//
+//    @NonNull
+//    @Size(min = 2, max = 30, message = "Name (2-30 characters)")
+//    private String name;
+//    @NotEmpty
+//    @Size(min=5)
+//
+//    public Club(String name, String president, int memberCount){
+//        this.name = name;
+//        this.president = president;
+//        this.memberCount = memberCount;
 //    }
-}
+//
+////    public int getMemberCount(){
+////        if(this.memberCount != null){
+////            //modify date code to this case
+////        }
+////    }
+//}
