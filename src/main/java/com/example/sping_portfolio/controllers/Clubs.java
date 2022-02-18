63eb143a4b675d6eda4d4d09be69312333dfa435
sam <70492417+dsblack0@users.noreply.github.com>
2022-02-18 09:55:32 -0800

    fixing homePage formatting

--- src/main/java/com/example/sping_portfolio/controllers/Clubs.java
+++ src/main/java/com/example/sping_portfolio/controllers/Clubs.java
@@ -53,42 +53,4 @@ public class Clubs implements Serializable {
     }
 
 
-    @GetMapping("/")
-    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String index(Model model) {
-
-        Clubs club1 = new Clubs();//creating an object of Student
-        Clubs club2 = new Clubs();
-        Clubs club3 = new Clubs();
-        Clubs club4 = new Clubs();//creating an object of Student
-        Clubs club5 = new Clubs();
-        Clubs club6 = new Clubs();
-        Clubs club7 = new Clubs();//creating an object of Student
-        Clubs club8 = new Clubs();
-
-        club1.clubCreate("Speech & Debate", "Participate in tournaments.", "Mondays & Wednesdays");
-        club2.clubCreate("Speech & Debate", "Participate in tournaments.", "Mondays & Wednesdays");
-        club3.clubCreate("Speech & Debate", "Participate in tournaments.", "Mondays & Wednesdays");
-        club4.clubCreate("Speech & Debate", "Participate in tournaments.", "Mondays & Wednesdays");
-        club5.clubCreate("Speech & Debate", "Participate in tournaments.", "Mondays & Wednesdays");
-        club6.clubCreate("Speech & Debate", "Participate in tournaments.", "Mondays & Wednesdays");
-        club7.clubCreate("Speech & Debate", "Participate in tournaments.", "Mondays & Wednesdays");
-        club8.clubCreate("Speech & Debate", "Participate in tournaments.", "Mondays & Wednesdays");
-
-        ArrayList<Clubs> list = new ArrayList<>();
-        list.add(club1);
-        list.add(club2);
-        list.add(club3);
-        list.add(club4);
-        list.add(club5);
-        list.add(club6);
-        list.add(club7);
-        list.add(club8);
-        model.addAttribute("list", list);
-
-        return "index"; // returns HTML VIEW (greeting)
-
-    }
-
-
 }
