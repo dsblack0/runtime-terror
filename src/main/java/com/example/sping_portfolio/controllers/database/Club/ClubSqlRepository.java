7c6642722a7f1a30ece4e93456c3fc805b269d8d
adritac-tech <adrita.chakraborty@outlook.com>
2022-02-18 09:51:01 -0800

    attempt at fixing

--- src/main/java/com/example/sping_portfolio/controllers/database/Club/ClubSqlRepository.java
+++ src/main/java/com/example/sping_portfolio/controllers/database/Club/ClubSqlRepository.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+//import javax.transaction.Transactional;
 import java.util.List;
 @Service
 @Transactional //ask mr m abt these two
@@ -22,11 +22,11 @@ public class ClubSqlRepository {
         jpa.save(club);
     }
 
-    public Club get(long id){
-        return (jpa.findById(id).isPresent())
-                ? jpa.findById(id.get())
-                : null;
-    }
+//    public Club get(long id){
+//        return (jpa.findById(id).isPresent())
+//                ? jpa.findById(id.get())
+//                : null;
+//    }
 
     public void delete(long id) {
         jpa.deleteById(id);
