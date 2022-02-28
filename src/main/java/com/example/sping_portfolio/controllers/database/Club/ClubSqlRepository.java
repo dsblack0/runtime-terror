ef2998b3d8e75f82eabb18bfbed8c0643e2918ec
rachelklee <iamrachelklee@gmail.com>
2022-02-27 21:23:39 -0800

    beans beans eat more beans

--- src/main/java/com/example/sping_portfolio/controllers/database/Club/ClubSqlRepository.java
+++ src/main/java/com/example/sping_portfolio/controllers/database/Club/ClubSqlRepository.java
@@ -1,7 +1,6 @@
 package com.example.sping_portfolio.controllers.database.Club;
 
 //import com.nighthawk.csa.database.scrum.ScrumSqlRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,7 +9,6 @@ import java.util.List;
 @Service
 @Transactional //ask mr m abt these two
 public class ClubSqlRepository {
-    @Autowired
     private ClubJpaRepository jpa;
 
     public List<Club>listAll(){
